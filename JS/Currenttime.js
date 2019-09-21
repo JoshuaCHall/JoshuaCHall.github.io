b9d06d235385eb4852a2d235391249d8342aa443
JoshuaCHall <jch212@byui.edu>
2019-09-21 15:11:10 -0600

    time

---
+++ JS/Currenttime.js
@@ -0,0 +1,7 @@
+function GetTime() {
+    var modiDate = new Date();
+    var CurTime = modiDate.getHours() + ":" + modiDate.getMinutes()
+    return CurTime
+}
+document.write(GetTime());
+document.write("");
