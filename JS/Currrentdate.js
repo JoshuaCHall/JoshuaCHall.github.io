907e4e2e7a53a5f625be55ac9973e3f26d5a11dc
JoshuaCHall <jch212@byui.edu>
2019-09-21 11:59:17 -0600

    fixed date

--- JS/Currrentdate.js
+++ JS/Currrentdate.js
@@ -1,2 +1,2 @@
     const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', time: 'numeric'};
-    document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+    document.getElementById('Currentdate').textContent = new Date().toLocaleDateString('en-US', options);
