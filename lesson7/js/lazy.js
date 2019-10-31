996bdb9921fd17ac3e51d60cfced566c4b6c13fa
JoshuaCHall <jch212@byui.edu>
2019-10-30 23:32:01 -0600

    changes

--- lesson7/js/lazy.js
+++ lesson7/js/lazy.js
@@ -1,5 +1,42 @@
-const images = document.querySelectorAll("[data-src]");
+const images = document.querySelectorAll("img[data-src]");
 
 const imgoptions = {};
 
-const imgobserver = new 
+const imgobserver = new IntersectionObserver((entries,imgobserver )=>{
+entries.forEach(entry) => {
+    if (!entry.intersection) {
+        return
+    } else {
+preloadImage(entrytarget);
+ingobserver.onobserve(entrytarget)
+    }
+})
+
+}, imgoptions);
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
