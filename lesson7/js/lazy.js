635ba711a76fed3f22ba156eeb86988b097797ed
JoshuaCHall <jch212@byui.edu>
2019-10-31 20:52:18 -0600

    js code fixed

--- lesson7/js/lazy.js
+++ lesson7/js/lazy.js
@@ -1,43 +1,32 @@
-const images = document.querySelectorAll("img[data-src]");
+let imagestoLoad = document.querySelectorAll("img[data-src]");
 
-const imgoptions = {};
 
-const imgobserver = new IntersectionObserver((entries,imgobserver )=>{
-entries.forEach(entry) => {
-    if (!entry.intersection) {
-        return
-    } else {
-preloadImage(entrytarget);
-ingobserver.onobserve(entrytarget)
-    }
-})
-
-}, imgoptions);
-if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-      items.forEach((item) => {
-        if(item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
-      });
-    });
-    imagesToLoad.forEach((img) => {
-      observer.observe(img);
-    });
-  } else {
-    imagesToLoad.forEach((img) => {
-      loadImages(img);
-      
-
-let imagesToLoad = document.querySelectorAll('img[data-src]');
 const loadImages = (image) => {
-  image.setAttribute('src', image.getAttribute('data-src'));
-  image.onload = () => {
-    image.removeAttribute('data-src');
-  };
-};
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {image.removeAttribute('data-src');};
+    };
 
-imagesToLoad.forEach((img) => {
+const imageOptions = {
+      threshold: 1,
+      rootMargin: "0px 0px 50px 0px"
+  };
+
+if('IntersectionObserver' in window){
+const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+          if(item.isIntersecting){
+            loadImages(item.target);
+            imgObserver.unobserve(item.target);
+          }
+        });
+    });
+
+imagestoLoad.forEach((img) => {
+    imgObserver.observe(img);
+});
+}
+else {
+  imagestoLoad.forEach((img) => {
     loadImages(img);
   });
+}
