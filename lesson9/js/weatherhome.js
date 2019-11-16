76e49bb653a1fd0f5c06e8c0e37e1ebe527ce0e5
JoshuaCHall <jch212@byui.edu>
2019-11-16 16:30:40 -0700

    js starting

--- lesson9/js/weatherhome.js
+++ lesson9/js/weatherhome.js
@@ -1,31 +1,47 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
-
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    const prophets = jsonObject['prophets'];
-    //console.table(jsonObject);  // temporary checking for valid response and data parsing
-  for (let i = 0; i < prophets.length; i++ ) {
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let birthdate = document.createElement('p');
-    let birthplace = document.createElement('p');
-    let image = document.createElement('img');
-    h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-    birthdate.textContent = 'Date of Birth:' + prophets[i].birthdate;
-    birthplace.textContent = 'Place of Birth:' + prophets[i].birthplace;
-    image.textContent = prophets[i].name + ' ' + prophets[i].imageurl;
-    image.setAttribute('src', prophets[i].imageurl);
-    image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + prophets[i].order);
-    card.appendChild(h2);
-    card.appendChild(birthdate);
-    card.appendChild(birthplace);
-    card.appendChild(image);
-    document.querySelector('div.cards').appendChild(card);
-  }
-});  
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+fetch(requestURL) 
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject); //temporary testing
+        const towns = jsonObject['towns'];
+        const idahoTowns = ["Fish Haven", "Soda Springs", "Preston"];
+        for (let i = 0; i < towns.length; i++) { 
+            if (idahoTowns.includes(towns[i].name)) {
+            //create variable elements
+            let card = document.createElement('section');
+            let span = document.createElement('span');
+            let h3 = document.createElement('h3');
+            let motto = document.createElement('i');
+            let yearFounded = document.createElement('p');
+            let currentPopulation = document.createElement('p');
+            let averageRainfall = document.createElement('p');
+            let image = document.createElement('img');
+            //modify the contents
+            card.className = 'towncard';
+            motto.className = 'motto';
+            h3.textContent = towns[i].name;
+            motto.textContent = towns[i].motto;
+            yearFounded.textContent =  'Year Founded: ' + towns[i].yearFounded;
+            currentPopulation.textContent =  'Population: ' + towns[i].currentPopulation;
+            averageRainfall.textContent =  'Annual Rain Fall: ' + towns[i].averageRainfall;
+            image.setAttribute('src', 'images/' + towns[i].photo);
+            image.setAttribute('alt', towns[i].name);
+            //Merge them together
+            
+            span.appendChild(h3);
+            span.appendChild(motto);
+            span.appendChild(yearFounded);
+            span.appendChild(currentPopulation);
+            span.appendChild(averageRainfall);
+            card.appendChild(span);
+            card.appendChild(image);
+            //Attaching to the page
+            document.querySelector('div.towncards').appendChild(card);
+        }
+        }
+    });
     
     
 
