034cdd0168fd1ec465fdc7aaf2c095b4f273b60a
JoshuaCHall <jch212@byui.edu>
2019-11-15 21:29:54 -0700

    changes

--- lesson9/js/weatherhome.js
+++ lesson9/js/weatherhome.js
@@ -5,14 +5,14 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    const prophets = jsonObject['prophets'];
+    const prophets = jsonObject['homepage'];
     //console.table(jsonObject);  // temporary checking for valid response and data parsing
   for (let i = 0; i < prophets.length; i++ ) {
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
     let birthdate = document.createElement('p');
     let birthplace = document.createElement('p');
-    let image = document.createElement('img');
+    let image = document.createElement('home900jpg');
     h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
     birthdate.textContent = 'Date of Birth:' + prophets[i].birthdate;
     birthplace.textContent = 'Place of Birth:' + prophets[i].birthplace;
